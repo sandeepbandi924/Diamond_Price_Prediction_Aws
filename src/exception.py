6a7082ce99868088e8d59312e3a3bea1ae00760c
sandeepbandi924 <bandisandeep1374@gmail.com>
2023-09-20 23:36:25 +0530

    Logging and Exception1

--- src/exception.py
+++ src/exception.py
@@ -21,11 +21,11 @@ class CustomException(Exception):
         return self.error_message    
     
 
-if __name__=="__main__":
-    logging.info("Logging has started")
+# if __name__=="__main__":
+#     logging.info("Logging has started")
 
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info('Division by zero') 
-        raise CustomException(e,sys)
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info('Division by zero') 
+#         raise CustomException(e,sys)
